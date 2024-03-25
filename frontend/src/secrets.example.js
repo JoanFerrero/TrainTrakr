c9dc54bce1f71dd0e73e05f675b68f4bc2e77f3b
JoanFerrero <joan.ferrero.montiel@gmail.com>
2024-03-25 12:31:16 +0100

    Change

--- frontend/src/secrets.example.js
+++ frontend/src/secrets.example.js
@@ -5,7 +5,11 @@ const secrets = {
   projectId: "",
   storageBucket: "",
   messagingSenderId: "",
-  appId: ""
+  appId: "",
+  YOUR_TEMPLATE_ID_CONTACT: "",
+  YOUR_TEMPLATE_ID_REGISTER: "",
+  YOUR_SERVICE_ID: "",
+  YOUR_PUBLIC_KEY: "",
 }
 
 export default secrets;
